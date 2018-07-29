85d142c01c3b8878970691e0feb9f18677ecf588
Les Matheson <les.matheson@gmail.com>
2018-07-28 21:59:01 -0400

    sync

--- test1.py
+++ test1.py
@@ -2,11 +2,34 @@
 
 from shelljob import proc, job
 import sys
+from multiprocessing import Process, Pipe
+from subprocess import check_output,Popen
+import subprocess
+from time import sleep
 
 cout=sys.stdout.write
 
 g=None
 rx=None
+
+def xstub(*args):
+    sys.stderr.write("xstub: %s\n" % str(args))
+
+def very_long_ls(dirname, tty, mypipe):
+    xstub("very_long_ls", dirname, tty )
+    try:
+        with open(tty, 'w') as ttyout:
+            p=Popen(['/bin/ls','-alR', dirname], stdout=ttyout,stderr=ttyout)
+            mypipe.send(["ls is running"])
+            print "child rx:", mypipe.recv()
+            while p.poll():
+                sleep(0.3)
+
+
+        p.wait()
+    except BaseException as e:
+        print("Caught: " + str(e))
+    print("very_long_ls exiting")
 
 def dump():
     global rx
@@ -22,5 +45,24 @@ def  test_1():
 
 
 if __name__ == "__main__":
-    test_1()
-    print("Use dump() to call readlines")
+
+    parent_pipe, child_pipe=Pipe()
+    p = Process(target=very_long_ls,args=('/','/dev/ttys008',child_pipe))
+    p.start()
+    print("p is running: " + str(p.pid))
+
+    while p.is_alive():
+        res=raw_input("q?")
+        print parent_pipe.recv()
+        print("You said: " + res)
+        parent_pipe.send(["user said",res])
+
+    
+    p.join()
+    print("p=", p.pid)
+
+
+
+
+
+
