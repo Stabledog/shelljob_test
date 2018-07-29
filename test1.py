933e0253cb8b1ad2e3c2d404bce8b97f037ecafd
Les Matheson <les.matheson@gmail.com>
2018-07-29 19:03:21 -0400

    This finally works

--- test1.py
+++ test1.py
@@ -2,7 +2,7 @@
 
 from shelljob import proc, job
 import sys
-from multiprocessing import Process, Pipe
+from multiprocessing import Process, Pipe, Queue
 from subprocess import check_output,Popen
 import subprocess
 from time import sleep
@@ -11,21 +11,30 @@ cout=sys.stdout.write
 
 g=None
 rx=None
+xtty='/dev/ttys008'
 
 def xstub(*args):
     sys.stderr.write("xstub: %s\n" % str(args))
 
-def very_long_ls(dirname, tty, mypipe):
-    xstub("very_long_ls", dirname, tty )
+def very_long_ls(inq, outq):
+    # inq => send to child
+    # outq => send to parent
+    global xtty
+    dirname='/'
+    xstub("I say very_long_ls starting")
+    outq.put(["ls is running"])
     try:
-        with open(tty, 'w') as ttyout:
+        with open(xtty, 'w') as ttyout:
             p=Popen(['/bin/ls','-alR', dirname], stdout=ttyout,stderr=ttyout)
-            mypipe.send(["ls is running"])
-            print "child rx:", mypipe.recv()
-            while p.poll():
-                sleep(0.3)
-
-
+            sleep(2)
+            while p.poll() == None:
+                r=inq.get()
+                xstub("parent said:",r)
+                if r[1]=='quit':
+                    break
+                outq.put(["hi.  You said:",r])
+                sleep(1)
+        xstub("leaving inner loop")
         p.wait()
     except BaseException as e:
         print("Caught: " + str(e))
@@ -46,16 +55,18 @@ def  test_1():
 
 if __name__ == "__main__":
 
-    parent_pipe, child_pipe=Pipe()
-    p = Process(target=very_long_ls,args=('/','/dev/ttys008',child_pipe))
+    parentq=Queue() # send to parent
+    childq=Queue()  # send to child
+    p = Process(target=very_long_ls,args=(childq,parentq))
     p.start()
     print("p is running: " + str(p.pid))
 
     while p.is_alive():
+        r=parentq.get()
+        print("child said:",r)
+
         res=raw_input("q?")
-        print parent_pipe.recv()
-        print("You said: " + res)
-        parent_pipe.send(["user said",res])
+        childq.put(["user said:",res])
 
     
     p.join()
